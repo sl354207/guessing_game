d152cc276a9fff8e151ea7595f3a2d3db2da91a1
sl354207 <sl354207@ohio.edu>
2022-12-24 21:03:44 -0500

    Handle user input

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+// import io library
+use std::io;
+
 fn main() {
-    println!("Hello, worlds!");
+    // print directions
+    println!("Guess the number!");
+    println!("Input your guess!");
+
+    // create mutable empty string
+    let mut guess = String::new();
+
+    // handle user input
+    io::stdin()
+        .read_line(&mut guess) //append to guess var
+        .expect("failed to read line"); //handle error if present
+
+    // print input
+    println!("You guessed: {guess}");
 }
